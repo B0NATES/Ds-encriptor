b94d1912235d0a20caf292a57358a700dc2ec4ad
Bonates <natanbonatescursos@hotmail.com>
2024-01-23 18:31:00 -0300

    commit inicial

---
+++ js/apresenta.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const texto = "Bem-vindo ao DC!"; 
+    const delay = 40; 
+    const container = document.getElementById('texto-digitar');
+
+    function digitarCaracteres(index) {
+        if (index < texto.length) {
+            container.innerHTML += texto.charAt(index);
+            index++;
+            setTimeout(function() {
+                digitarCaracteres(index);
+            }, delay);
+        }
+    }
+
+    digitarCaracteres(0);
+});
